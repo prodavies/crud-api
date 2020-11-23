f734c41ce6e163b9183d33e9348a9e7d9d6e5f4e
prodavies <39768603+prodavies@users.noreply.github.com>
2020-11-23 12:34:45 +0300

    Database config

--- config/database.php
+++ config/database.php
@@ -1,3 +1,5 @@
+
+$DATABASE_URL=parse_url(‘postgres://yyalyksjzcnnhc:24fd677c96d6d82ea3e667c2ba11fca1b89c7cae041d72d663f0bb370eb96da5@ec2-23-20-168-40.compute-1.amazonaws.com:5432/d9g29ok8q7q2fc’);
 <?php
 
 use Illuminate\Support\Str;
@@ -15,7 +17,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -65,12 +67,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $DATABASE_URL["host"],
+            'port' => $DATABASE_URL["port"],
+            'database' => ltrim($DATABASE_URL["path"],"/"),
+            'username' => $DATABASE_URL["user"],
+            'password' => $DATABASE_URL["pass"],
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
